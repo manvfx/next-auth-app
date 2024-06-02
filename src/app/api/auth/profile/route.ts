a9b5e7fd6cb1a40d83e60ab6cdb956fb64ce643e
manvfx <manvfx@gmail.com>
2024-06-02 21:43:36 +0330

    fix profile route

--- src/app/api/auth/profile/route.ts
+++ src/app/api/auth/profile/route.ts
@@ -1,4 +1,4 @@
-import { verify } from "jsonwebtoken";
+import { verify, JwtPayload } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { COOKIE_NAME } from "@/constant";
 import { NextResponse } from "next/server";
@@ -9,19 +9,32 @@ export async function GET() {
   const jwtToken = cookieStore.get(COOKIE_NAME);
 
   if (!jwtToken) {
-    return NextResponse.json({ message: "Unauthorizaed" }, { status: 401 });
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   const { value } = jwtToken;
   const jwtSecret = process.env.JWT_SECRET || "";
 
   try {
-    const result = verify(value, jwtSecret);
-    const userFromDB = await User.findOne({ email: result.email });
-    const response = {
-      user: { email: userFromDB.email, name: userFromDB.name },
-    };
-    return NextResponse.json(response, { status: 200 });
+    const result = verify(value, jwtSecret) as JwtPayload;
+    if (typeof result === "object" && "email" in result) {
+      const userFromDB = await User.findOne({ email: result.email });
+      if (!userFromDB) {
+        return NextResponse.json(
+          { message: "User not found" },
+          { status: 404 }
+        );
+      }
+      const response = {
+        user: { email: userFromDB.email, name: userFromDB.name },
+      };
+      return NextResponse.json(response, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { message: "Invalid token payload" },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     return NextResponse.json({ message: "Unknown error" }, { status: 400 });
   }
